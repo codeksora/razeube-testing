293b1237bfed79b10694effdd90e1f5a4946d08a
Carlos Chirito <cchirito@aynitech.com>
2023-03-28 18:33:50 -0500

    Update curso

--- cypress/e2e/razeube/curso.cy.js
+++ cypress/e2e/razeube/curso.cy.js
@@ -20,6 +20,8 @@ describe('curso', () => {
     cy.get('[data-cy="Precio"]').type('19.56')
     cy.get('[data-cy="Link video 1"]').type('https://google.com')
     cy.get('[data-cy="Descripción 1"]').type('lorem asd sdsdsd ')
+    //id_teacher_id
+    // cy.get('[id="id_teacher_id"]').type('1')
     cy.get('[data-cy="Descripción de temario"]').type('lorem asd sdsdsd ')
     cy.get('[data-cy="id_image_temario"]').selectFile('cypress/fixtures/order.pdf', { action: 'drag-drop' })
 
@@ -53,7 +55,7 @@ describe('curso', () => {
     cy.get('[data-cy="certificaciones"] [data-cy="btn_agregar"]').click()
     cy.get('[data-cy="certificaciones"] [data-cy=galeriesimages2]').selectFile('cypress/fixtures/600x600.png', { action: 'drag-drop' })
 
-    cy.get('[data-cy="Guardar"]').click()
+    // cy.get('[data-cy="Guardar"]').click()
     // cy.wait(2000)
     // cy.get('.swal2-confirm').click()
   })
